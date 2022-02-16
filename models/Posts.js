9933c897e5de2fff2b82c56ae2cb440cabda015d
tkivai <tonykivai@gmail.com>
2022-02-16 13:08:05 +0300

    Define Post Schema

--- models/Posts.js
+++ models/Posts.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 
-const PostSchema = mongoose.Schema({})
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+})
 
 const Post = mongoose.model("Post", PostSchema, "posts");
 
